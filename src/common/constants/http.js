d22e1ae5e6d26a8d6f3f3432ff5c2c12c3720767
Kapic007 <83466803+AlexKapic@users.noreply.github.com>
2021-10-13 17:26:03 +0300

    create http.service

---
+++ src/common/constants/http.js
@@ -0,0 +1,18 @@
+export const HttpHeader = {
+  CONTENT_TYPE: "Content-Type",
+};
+
+export const HttpMethod = {
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  DELETE: "DELETE",
+};
+
+export const ContentType = {
+  JSON: "application/json",
+};
+
+export const URL = {
+  API: "https://fakestoreapi.com/",
+};
---
+++ src/services/http.service.js
@@ -0,0 +1,58 @@
+import {
+  HttpHeader,
+  HttpMethod,
+  ContentType,
+  URL,
+} from "../common/constants/http";
+
+class Http {
+  async load(url, options = {}) {
+    const { method = HttpMethod.GET, payload = null, contentType } = options;
+
+    const headers = this._getHeaders({ contentType });
+
+    try {
+      const response = await fetch(`${URL.API}+${url}`, {
+        method,
+        headers,
+        body: payload,
+      });
+
+      const response_1 = await this._checkStatus(response);
+
+      if (response.status === 204) {
+        return;
+      } else {
+        return this._parseJSON(response_1);
+      }
+    } catch (err) {
+      return this._throwError(err);
+    }
+  }
+
+  _getHeaders({ contentType }) {
+    const headers = new Headers();
+    if (contentType) {
+      headers.append(HttpHeader.CONTENT_TYPE, contentType);
+    }
+    return headers;
+  }
+
+  async _checkStatus(response) {
+    if (!response.ok) {
+      const parsedException = await response.json();
+      throw new Error(parsedException?.message ?? response.statusText);
+    }
+    return response;
+  }
+
+  _parseJSON(response) {
+    return response.json();
+  }
+
+  _throwError(err) {
+    throw err;
+  }
+}
+
+export { Http };
